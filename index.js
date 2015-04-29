64ba65e28bacea7b825b8039d86ee5e1127339b4
Nathan Ward <ward@digivine.com>
2015-04-29 14:37:44 -0700

    Add PouchDB as Express middleware
    This implementation currently requires locationType: 'hash' in the consuming application.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* jshint node: true */
 'use strict';
 
+var Pouch = require('pouchdb');
+var ExpressPouch = require('express-pouchdb')
+
 module.exports = {
-  name: 'bf-server'
+  name: 'bf-server',
+  serverMiddleware: function(config) {
+    var app = config.app;
+    var options = config.options;
+    var project = options.project;
+    app.use('/db', ExpressPouch(Pouch));
+  }
 };
